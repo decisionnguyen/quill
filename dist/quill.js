562d155f84df3d73b826b15b2779bba460f0533c
@decisionnguyen <decisionnguyen@gmail.com>
2020-05-06 11:16:06 +0700

    add space

--- dist/quill.js
+++ dist/quill.js
@@ -5012,27 +5012,25 @@ function handleDeleteRange(range) {
 }
 
 function handleEnter(range, context) {
-  var _this3 = this;
-
-  if (range.length > 0) {
-    this.quill.scroll.deleteAt(range.index, range.length); // So we do not trigger text-change
-  }
-  var lineFormats = Object.keys(context.format).reduce(function (lineFormats, format) {
-    if (_parchment2.default.query(format, _parchment2.default.Scope.BLOCK) && !Array.isArray(context.format[format])) {
-      lineFormats[format] = context.format[format];
-    }
-    return lineFormats;
-  }, {});
-  this.quill.insertText(range.index, '\n ', lineFormats, _quill2.default.sources.USER);
-  this.quill.setSelection(range.index + 2, _quill2.default.sources.SILENT);
-  // Earlier scroll.deleteAt might have messed up our selection,
-  // so insertText's built in selection preservation is not reliable
-  Object.keys(context.format).forEach(function (name) {
-    if (lineFormats[name] != null) return;
-    if (Array.isArray(context.format[name])) return;
-    if (name === 'link') return;
-    _this3.quill.format(name, context.format[name], _quill2.default.sources.USER);
-  });
+  // if (range.length > 0) {
+  //   this.quill.scroll.deleteAt(range.index, range.length);  // So we do not trigger text-change
+  // }
+  // let lineFormats = Object.keys(context.format).reduce(function(lineFormats, format) {
+  //   if (Parchment.query(format, Parchment.Scope.BLOCK) && !Array.isArray(context.format[format])) {
+  //     lineFormats[format] = context.format[format];
+  //   }
+  //   return lineFormats;
+  // }, {});
+  // this.quill.insertText(range.index, '\n ', lineFormats, Quill.sources.USER);
+  // this.quill.setSelection(range.index + 2, Quill.sources.SILENT);
+  // // Earlier scroll.deleteAt might have messed up our selection,
+  // // so insertText's built in selection preservation is not reliable
+  // Object.keys(context.format).forEach((name) => {
+  //   if (lineFormats[name] != null) return;
+  //   if (Array.isArray(context.format[name])) return;
+  //   if (name === 'link') return;
+  //   this.quill.format(name, context.format[name], Quill.sources.USER);
+  // });
   return true;
 }
 
--- modules/keyboard.js
+++ modules/keyboard.js
@@ -394,25 +394,25 @@ function handleDeleteRange(range) {
 }
 
 function handleEnter(range, context) {
-  if (range.length > 0) {
-    this.quill.scroll.deleteAt(range.index, range.length);  // So we do not trigger text-change
-  }
-  let lineFormats = Object.keys(context.format).reduce(function(lineFormats, format) {
-    if (Parchment.query(format, Parchment.Scope.BLOCK) && !Array.isArray(context.format[format])) {
-      lineFormats[format] = context.format[format];
-    }
-    return lineFormats;
-  }, {});
-  this.quill.insertText(range.index, '\n ', lineFormats, Quill.sources.USER);
-  this.quill.setSelection(range.index + 2, Quill.sources.SILENT);
-  // Earlier scroll.deleteAt might have messed up our selection,
-  // so insertText's built in selection preservation is not reliable
-  Object.keys(context.format).forEach((name) => {
-    if (lineFormats[name] != null) return;
-    if (Array.isArray(context.format[name])) return;
-    if (name === 'link') return;
-    this.quill.format(name, context.format[name], Quill.sources.USER);
-  });
+  // if (range.length > 0) {
+  //   this.quill.scroll.deleteAt(range.index, range.length);  // So we do not trigger text-change
+  // }
+  // let lineFormats = Object.keys(context.format).reduce(function(lineFormats, format) {
+  //   if (Parchment.query(format, Parchment.Scope.BLOCK) && !Array.isArray(context.format[format])) {
+  //     lineFormats[format] = context.format[format];
+  //   }
+  //   return lineFormats;
+  // }, {});
+  // this.quill.insertText(range.index, '\n ', lineFormats, Quill.sources.USER);
+  // this.quill.setSelection(range.index + 2, Quill.sources.SILENT);
+  // // Earlier scroll.deleteAt might have messed up our selection,
+  // // so insertText's built in selection preservation is not reliable
+  // Object.keys(context.format).forEach((name) => {
+  //   if (lineFormats[name] != null) return;
+  //   if (Array.isArray(context.format[name])) return;
+  //   if (name === 'link') return;
+  //   this.quill.format(name, context.format[name], Quill.sources.USER);
+  // });
   return true
 }
 
